13fca78f82a50556c081b433f90dbaa3d773ee3d
Jimena <jimena.qn26@gmail.com>
2024-06-13 23:55:01 -0500

    feature: navbar done

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css', '../menu-icons/menu-icons.component.css']
+})
+export class NavbarComponent {
+  // Código del componente si es necesario
+}
