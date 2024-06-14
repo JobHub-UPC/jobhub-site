a45ef9443bc628f93a6de1942f6752a904b5037c
AndresCRs <andresvmlp@gmail.com>
2024-06-14 06:03:51 -0500

    feat: se agregó el Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenuIconsComponent } from './menu-icons/menu-icons.component';
-
-import { MatMenuModule } from '@angular/material/menu';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent, // Asegúrate de incluir el componente aquí
     NavbarComponent,
-    MenuIconsComponent
+    MenuIconsComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
